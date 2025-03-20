7e0fdf8d961582951997c100c442c6b6b0705ff2
Krisna Rusdiono <krisnarusdiono@gmail.com>
2025-03-20 07:37:25 +0700

    fix: add route to homepage while clicking brand

--- components/layout/Header/index.tsx
+++ components/layout/Header/index.tsx
@@ -7,9 +7,11 @@ const Header = () => (
   <AppBar>
     <Toolbar variant='regular'>
       <AppContainer className='flex gap-20 justify-between items-center'>
-        <Typography variant='h6' className='!font-extrabold'>
-          STAYKUY
-        </Typography>
+        <Link href="/">
+          <Typography variant='h6' className='!font-extrabold'>
+            STAYKUY
+          </Typography>
+        </Link>
         <div className='w-full text-right flex gap-4 justify-end'>
           {MENU.map(({ id, path, name }) => (
             <Link href={path} key={id}>
@@ -17,9 +19,11 @@ const Header = () => (
             </Link>
           ))}
         </div>
-        <div className="flex gap-4 items-center">
-            <Typography className='w-10 h-10 flex justify-center items-center bg-neutral-700 text-white box-border rounded-full text-xl'>T</Typography>
-            <Typography variant="h6">ID</Typography>
+        <div className='flex gap-4 items-center'>
+          <Typography className='w-10 h-10 flex justify-center items-center bg-neutral-700 text-white box-border rounded-full text-xl'>
+            T
+          </Typography>
+          <Typography variant='h6'>ID</Typography>
         </div>
       </AppContainer>
     </Toolbar>
